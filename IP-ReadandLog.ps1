1319923bff9b60b6e14ac619eda4d0eef6873851
synapsentester <synapsentester@googlemail.com>
2025-09-06 08:52:56 +0200

    j judge me

--- IP-ReadandLog.ps1
+++ IP-ReadandLog.ps1
@@ -1,9 +1,22 @@
 ﻿$PathLOG = "C:\LocLogFile\"
-$IPLOG = $PathLOG +"IP-$Stamp.log"
-Test-Path -Path $PathLOG
-#Start-Sleep -Seconds 30
-$Stamp = Get-Date -Format yyyyMMdd-hhmmss
+$Stamp = (Get-Date).ToString("yyyyMMdd-HHmmss")
+$IPLOG = $PathLOG + "IP-$Stamp.log"
 
-ipconfig -all | Out-File -FilePath $IPLOG
-date | Out-File -FilePath $IPLOG -Append
+# Verzeichnis erstellen, falls nicht vorhanden
+if (-not (Test-Path -Path $PathLOG)) {
+    New-Item -ItemType Directory -Path $PathLOG | Out-Null
+}
+
+"==== Benutzer ====" | Out-File -FilePath $IPLOG
 $env:USERNAME | Out-File -FilePath $IPLOG -Append
+
+"`n==== Datum ====" | Out-File -FilePath $IPLOG -Append
+(Get-Date) | Out-File -FilePath $IPLOG -Append
+
+"`n==== Computername ====" | Out-File -FilePath $IPLOG -Append
+$env:COMPUTERNAME | Out-File -FilePath $IPLOG -Append
+
+"`n==== IP-Konfiguration ====" | Out-File -FilePath $IPLOG -Append
+ipconfig /all | Out-File -FilePath $IPLOG -Append
+
+Start-Process $IPLOG
